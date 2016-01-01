b3b5734f8dbf77c82082fd7f5e823eff5da0294a
Vn0m <youssef.azzaoui@epfl.ch>
2016-01-01 13:55:07 +0100

    -  travaux fais sur la la partie google map

--- app/src/main/java/azzaoui/sociadee/MapWrapperFragment.java
+++ app/src/main/java/azzaoui/sociadee/MapWrapperFragment.java
@@ -2,17 +2,34 @@ package azzaoui.sociadee;
 
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapWrapperFragment extends Fragment {
     GoogleMap map;
@@ -50,8 +67,82 @@ public class MapWrapperFragment extends Fragment {
 
         if (map == null) {
             map = mapFragment.getMap();
-            map.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+            // Changing map type
+            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            // Showing/hiding your current location
+            map.setMyLocationEnabled(false);
+            // Enable/disable zooming controls
+            map.getUiSettings().setZoomControlsEnabled(false);
+            // Enable/disable my location button
+            map.getUiSettings().setMyLocationButtonEnabled(true);
+            // Enable/disable compass icon
+            map.getUiSettings().setCompassEnabled(false);
+            // Enable/disable rotate gesture
+            map.getUiSettings().setRotateGesturesEnabled(false);
+            // Enable/disable zooming functionality
+            map.getUiSettings().setZoomGesturesEnabled(true);
+            LocationManager locationManager;
+            String context = Context.LOCATION_SERVICE;
+            locationManager = (LocationManager) getActivity().getSystemService(context);
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingRequired(false);
+            criteria.setCostAllowed(true);
+            criteria.setPowerRequirement(Criteria.POWER_LOW);
+            String provider = locationManager.getBestProvider(criteria, true);
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+                Location location = locationManager.getLastKnownLocation(provider);
+                if (location != null) {
+                    updateWithNewLocation(location);
+                }
+            }
+            locationManager.requestLocationUpdates(provider, 2000, 10,locationListener);
+
         }
     }
 
+    private void updateWithNewLocation(Location location) {
+
+        if(location != null){
+        // Update the map location
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(latitude, longitude)).zoom(17).build();
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            map.clear();
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(new LatLng(latitude, longitude));
+            markerOptions.title("Here I am");
+            markerOptions.snippet("(" + latitude + "," + longitude + ")");
+            map.addMarker(markerOptions).showInfoWindow();
+
+            /*
+            Geocoder gc = new Geocoder(getActivity(), Locale.getDefault());
+            try
+            {
+                List<Address> addresses = gc.getFromLocation(latitude, longitude, 1);
+                StringBuilder sb = new StringBuilder();
+                if (addresses.size() > 0) {
+                    Address address = addresses.get(0);
+                    for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                        sb.append("\n").append(address.getAddressLine(i));
+                    }
+                    addressString = sb.toString();
+                }
+            }
+            catch (IOException e){}*/
+        }
+    }
+    private final LocationListener locationListener = new LocationListener(){
+                        public void onLocationChanged(Location location){
+                            updateWithNewLocation(location);
+                        }
+                        public void onProviderDisabled(String provider){
+                            updateWithNewLocation(null);
+                        }
+                        public void onProviderEnabled(String provider){}
+                        public void onStatusChanged(String provider, int status, Bundle extras){}
+                    };
 }
